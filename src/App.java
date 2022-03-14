6c1fe87c5caf2dcb4bc18acdc2358fd45933d7d1
novb1492 <novb1492@naver.com>
2022-03-14 23:10:52 +0900

    2단계 처음 부터 왜이리 어렵지?

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -7,16 +9,29 @@ public class App {
         System.out.println(solution(s));
     }
     public static int solution(String s) {
-        List<Integer>arr=new ArrayList<>();
+        Map<String,Integer>arr=new HashMap<>();
         int answer = 0;
         int len=s.length();
-        int num=0;
-        for(int i=1;i<len/2;i++){
+        for(int i=1;i<len;i++){
             for(int ii=0;ii<len;ii+=i){
                 //System.out.println("i:"+i);
-               // System.out.println("ii:"+ii);
-                System.out.println(s.substring(ii, ii+i));
+                //System.out.println("ii:"+ii);
+                int next=ii+i;
+                if(next>len-1){
+                    next=len-1;
+                }
+                String subString=s.substring(ii, next);
+                //System.out.println(subString);
+                if(arr.containsKey(subString)){
+                    int num=arr.get(subString);
+                    num+=1;
+                    arr.replace(subString, num);
+                }else{
+                    arr.put(subString, 1);
+                }
             }
+            System.out.println(arr.toString());
+            arr.clear();
         }
         return answer;
     }
