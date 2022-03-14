8beb23df7d9f83f8cbc19f3372a5f376c12648a6
kimjunyoung <kimjy80@sesisoft.com>
2022-03-14 17:26:10 +0900

    이렇게 행하나

--- src/App.java
+++ src/App.java
@@ -1,10 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
        String s="aabbaccc";
         System.out.println(solution(s));
     }
     public  static int solution(String s) {
+        List<Integer>arr=new ArrayList<>();
         int answer = 0;
+        int len=s.length();
+        char[] ss=s.toCharArray();
+        int num=0;
+        for(int i=1;i<=len;i++){
+           while(true){
+               int num2=num+i;
+                if(num2>len-1){
+                    num2=len-1;
+                }
+               System.out.println(s.substring(num, num2));
+               num+=i;
+               if(num2>=len-1){
+                   break;
+               }
+           }
+           num=0;
+        
+        }
         return answer;
     }
 }
