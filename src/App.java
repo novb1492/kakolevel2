84a8fb7b2c04bc0a86967b98ce1d098f22463383
novb1492 <novb1492@naver.com>
2022-03-14 22:37:15 +0900

    이렇게 하면 될거같기도

--- src/App.java
+++ src/App.java
@@ -6,26 +6,17 @@ public class App {
        String s="aabbaccc";
         System.out.println(solution(s));
     }
-    public  static int solution(String s) {
+    public static int solution(String s) {
         List<Integer>arr=new ArrayList<>();
         int answer = 0;
         int len=s.length();
-        char[] ss=s.toCharArray();
         int num=0;
-        for(int i=1;i<=len;i++){
-           while(true){
-               int num2=num+i;
-                if(num2>len-1){
-                    num2=len-1;
-                }
-               System.out.println(s.substring(num, num2));
-               num+=i;
-               if(num2>=len-1){
-                   break;
-               }
-           }
-           num=0;
-        
+        for(int i=1;i<len/2;i++){
+            for(int ii=0;ii<len;ii+=i){
+                //System.out.println("i:"+i);
+               // System.out.println("ii:"+ii);
+                System.out.println(s.substring(ii, ii+i));
+            }
         }
         return answer;
     }
