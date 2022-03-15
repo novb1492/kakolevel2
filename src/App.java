09515a5935e62f84fca7ac25554f12cf20eef7fe
kimjunyoung <kimjy80@sesisoft.com>
2022-03-15 10:05:09 +0900

    누락 따로 합쳐놓기

--- src/App.java
+++ src/App.java
@@ -6,15 +6,17 @@ import java.util.Optional;
 
 public class App {
     public static void main(String[] args) throws Exception {
-       String s="ababcdcdababcdcd";
+       String s="abcabcabcabcdededededede";
         System.out.println(solution(s));
     }
     public static int solution(String s) {
         Map<String,Integer>arr=new HashMap<>();
         int answer = s.length();
         int len=s.length();
+        int len2=0;
         for(int i=1;i<len/2;i++){
             String beforeString="";
+            String other="";
             for(int ii=0;ii<len;ii+=i){
                 //System.out.println("i:"+i);
                 //System.out.println("ii:"+ii);
@@ -31,11 +33,14 @@ public class App {
                 }else{
                     if(!arr.containsKey(subString)){
                         arr.put(subString, 1);
+                    }else{
+                        other=other+subString;
                     }
                     beforeString=subString;
                 }
             }
             System.out.println(arr.toString());
+            System.out.println(other);
             arr.clear();
         }
         return answer;
