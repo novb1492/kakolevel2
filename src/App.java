7f7fbd768060f59fdea696df90efec3fa352ca3e
kimjunyoung <kimjy80@sesisoft.com>
2022-03-15 11:17:35 +0900

    와 풀었다 문자열 압축 통과

--- src/App.java
+++ src/App.java
@@ -15,7 +15,6 @@ public class App {
         int answer = s.length();
         int len=s.length();
         for(int i=1;i<=len/2;i++){
-            String beforeString="";
             String other="";
             for(int ii=0;ii<len;ii+=i){
                // System.out.println("i:"+i);
@@ -33,7 +32,7 @@ public class App {
                         next2=len;
                     }
                     String subString2=s.substring(iii, next2);
-                    System.out.println(subString2);
+                  //  System.out.println(subString2);
                     if(subString.equals(subString2)){
                         num+=1;
                         ii=ii+i;
@@ -41,21 +40,18 @@ public class App {
                         break;
                     }
                 }
-                System.out.println(num);
+                if(num!=1){
+                    other=other+num+subString;
+                }else{
+                    other=other+subString;
+                }
             }
             System.out.println(arr.toString());
             System.out.println("other: "+other);
-            for(Entry<String, Integer> d:arr.entrySet()){
-                if(d.getValue()!=1){
-                    other=other+d.getValue()+d.getKey();    
-                }else{
-                    other=other+d.getKey();    
-                }
-            }
-            //System.out.println(other);
             if(other.length()<answer){
                 answer=other.length();
             }
+            System.out.println("len:"+answer);
             arr.clear();
         }
         return answer;
