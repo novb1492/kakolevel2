2b46ff7ec7c388a61b2de51ffbe662480ad8ff18
kimjunyoung <kimjy80@sesisoft.com>
2022-03-15 09:51:54 +0900

    거의온거같은데

--- src/App.java
+++ src/App.java
@@ -2,39 +2,41 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class App {
     public static void main(String[] args) throws Exception {
-       String s="aabbaccc";
+       String s="ababcdcdababcdcd";
         System.out.println(solution(s));
     }
     public static int solution(String s) {
         Map<String,Integer>arr=new HashMap<>();
-        int answer = 0;
+        int answer = s.length();
         int len=s.length();
-        for(int i=1;i<len;i++){
+        for(int i=1;i<len/2;i++){
             String beforeString="";
             for(int ii=0;ii<len;ii+=i){
                 //System.out.println("i:"+i);
                 //System.out.println("ii:"+ii);
                 int next=ii+i;
                 if(next>len){
-                    next=len-1;
+                    next=len;
                 }
                 String subString=s.substring(ii, next);
-               // System.out.println(subString);
-                if(beforeString.equals("")){
-                    beforeString=subString;
-                    ii=ii+i;
-                }else if(!beforeString.equals(subString)){
-                    beforeString=subString;
+                //System.out.println(subString);
+                if(beforeString.equals(subString)){
+                    int num=arr.get(beforeString);
+                    num=num+1;
+                    arr.put(beforeString, num);
                 }else{
-                    //System.out.println(subString);
-                    ii=ii+i;
+                    if(!arr.containsKey(subString)){
+                        arr.put(subString, 1);
+                    }
+                    beforeString=subString;
                 }
-                System.out.println(beforeString);
             }
-
+            System.out.println(arr.toString());
+            arr.clear();
         }
         return answer;
     }
