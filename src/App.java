ba71203facbfdc2fae72465349449d0d6ba1b869
kimjunyoung <kimjy80@sesisoft.com>
2022-03-15 11:25:32 +0900

    복습중

--- src/App.java
+++ src/App.java
@@ -17,12 +17,12 @@ public class App {
         for(int i=1;i<=len/2;i++){
             String other="";
             for(int ii=0;ii<len;ii+=i){
-               // System.out.println("i:"+i);
-               // System.out.println("ii:"+ii);
+                System.out.println("ii:"+ii);
                 int next=ii+i;
                 if(next>len){
                     next=len;
                 }
+                System.out.println("next:"+next);
                 String subString=s.substring(ii, next);
                 System.out.println("oriin:"+subString);
                 int num=1;
@@ -32,7 +32,7 @@ public class App {
                         next2=len;
                     }
                     String subString2=s.substring(iii, next2);
-                  //  System.out.println(subString2);
+                    System.out.println("sub2: "+subString2);
                     if(subString.equals(subString2)){
                         num+=1;
                         ii=ii+i;
@@ -46,12 +46,12 @@ public class App {
                     other=other+subString;
                 }
             }
-            System.out.println(arr.toString());
-            System.out.println("other: "+other);
+            //System.out.println(arr.toString());
+           // System.out.println("other: "+other);
             if(other.length()<answer){
                 answer=other.length();
             }
-            System.out.println("len:"+answer);
+            //System.out.println("len:"+answer);
             arr.clear();
         }
         return answer;
