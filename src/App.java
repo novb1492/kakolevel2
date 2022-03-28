b7075e045cc22e567ba748e72421356accb27e9a
kimjunyoung <kimjy80@sesisoft.com>
2022-03-28 18:00:53 +0900

    괄호변환 시작 겁나 오래걸릴듯..

--- src/App.java
+++ src/App.java
@@ -15,70 +15,12 @@ public class App {
     static int num2=0;
     static List<String>menuStrings=new ArrayList<>();
     public static void main(String[] args) throws Exception {
-       String[] s={"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
-       int[] c={2,3,4};
-        System.out.println("r: "+Arrays.toString(solution(s,c)));
+        String s="(()())()";
+        System.out.println("r: "+solution(s));
     }
-    public static String[] solution(String[] orders, int[] course) {
-        String[] answer = {};
-        menuArr=new ArrayList<>();
-        //경우의수가져오기
-        for(String order:orders){
-            char[] menus=order.toCharArray();
-            Arrays.sort(menus);
-            System.out.println(Arrays.toString(menus));
-            int len=menus.length;
-            for(int i=0;i<len;i++){
-                for(int c:course){
-                    dfs(c, menus,String.valueOf(menus[i]),menus[i]);
-                }
-            }
-
-        }
-        System.out.println(menuAndNums.toString());
-        //해당 길이중 가장 많이 된것 넣기
-        Map<Integer,Integer>topInLen=new HashMap<>();      
-        for(Entry<String, Integer> menuAndNum:menuAndNums.entrySet()){
-            int len=menuAndNum.getKey().length();
-            int topCount=Optional.ofNullable(topInLen.get(len)).orElseGet(()->0);
-            if(topCount<menuAndNum.getValue()){
-                topInLen.put(len, menuAndNum.getValue());
-            }
-        }
-        System.out.println(topInLen.toString());
-        //해당 메뉴중 가장 많이 주문된것 모두 추려내기
-        List<String>topMenusInLen=new ArrayList<>();
-        for(Entry<String, Integer> menuAndNum:menuAndNums.entrySet()){
-            if(menuAndNum.getValue()==topInLen.get(menuAndNum.getKey().length())&&menuAndNum.getValue()>=2){
-                topMenusInLen.add(menuAndNum.getKey());
-            }
-        }
-        //정렬
-        Collections.sort(topMenusInLen);
-        answer=topMenusInLen.toArray(new String[topMenusInLen.size()]);
+    public static String solution(String p) {
+        String answer = "";
         return answer;
     }
-    private static void dfs(int len,char[]menus,String str,char menu) {
-        //문자열 길이가 조건길이인지 확인
-        if(len==str.length()){
-            int count=Optional.ofNullable(menuAndNums.get(str)).orElseGet(()->0);
-            count++;
-            menuAndNums.put(str, count);
-            return;
-        }
-        //재귀호출
-        for(int i=0;i<menus.length;i++){
-            char a=menus[i];
-            //이미 있다면 혹은 이전 메뉴라면 무시 dfs 타지 않음
-            if(str.contains(String.valueOf(a))||a<menu){
-                continue;
-            }
-            dfs(len, menus, str+menus[i],menus[i]);
-            //System.out.println("다음스텝"); 
-        }
-
-    }
-    //abcfg
-    //0a1ab*
 
 }
