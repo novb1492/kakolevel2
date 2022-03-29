971c00fcf7f3579b9db7c3f7ead5eb2b926aebcb
kimjunyoung <kimjy80@sesisoft.com>
2022-03-29 11:04:55 +0900

    일단 분리를 하자 서로 같은 괄호수 까지

--- src/App.java
+++ src/App.java
@@ -19,19 +19,28 @@ public class App {
         char[] arr=p.toCharArray();
         int num=0;
         int num2=0;
-        String aa="";
-        for(char a:arr){
-            aa=aa+a;
-            if(a=='('){
+        String u="";
+        String v="";
+        int len=arr.length;
+        //괄호 수가 서로 같을 때까지 탐색
+        for(int i=0;i<len;i++){
+            char aaa=arr[i];
+            u=u+aaa;
+            if(aaa=='('){
                 num+=1;
             }else{
                 num2+=1;
             }
             if(num==num2){
-                System.out.println(aa);
+                //같다면 기준으로 분리
+                for(int ii=i+1;ii<len;ii++){
+                    v=v+arr[ii];
+                }
                 break;
             }
         }
+        System.out.println(u);
+        System.out.println(v);
         
         return answer;
     }
